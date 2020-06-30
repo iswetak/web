ac449804d163fb083850a28238d29c17507f19c8
iswetak <sweta@ruminationlabs.com>
2020-06-30 21:00:06 +0530

    basket-css

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ export default class App extends Component {
       render() {
         return (
       <div>
-         {/* <Basket/> */}
-       <Pulse/>
+         <Basket/>
+       {/* <Pulse/> */}
        {/* <News/> */}
       </div>
     );
