e94b727bae41f1569cdb50b326def50bb5a68423
iswetak <58159807+iswetak@users.noreply.github.com>
2020-07-08 17:06:29 +0530

    basket-detail-review

--- src/Newscomponent/NewsElement.js
+++ src/Newscomponent/NewsElement.js
@@ -5,52 +5,46 @@ import CSSModules from 'react-css-modules';
 import  styles from "./News.module.css";
 
 const CustomRow = ({ eventCategory,title,subtitle1, urlToImage, avatarURL, publishedAt, source, twitterHandle, twitterurl,url, subtitle3,sourceURL}) => (
-        <div className={styles.maincontainer} >
-                <div className={styles.separator}>
-                        <div style={{marginLeft:10, marginRight:10}}>
+        <div className={styles.news_main_container} >
+               
+                <div className={styles.top_header}>
+                <div className={styles.info_avatar}>
+                <img  className={styles.avatar} src={url= eventCategory=="NEWS"?"https://s3.ap-south-1.amazonaws.com/public.sicrobo/news-logos/"+source+".jpg"  :avatarURL} />        
+                <p className={styles.source}>{source}</p> 
+                </div>
+ 
+                <p  class={styles.publishedAt}>
+                { moment(publishedAt || moment.now()).format("hh:mm a")}           
+                </p>                       
+                </div>
 
-                        <div className={styles.topheader}>
-                        <div className={styles.avatard}>
-                        <img style={{height:30, width:40}} src={url= eventCategory=="NEWS"?"https://s3.ap-south-1.amazonaws.com/public.sicrobo/news-logos/"+source+".jpg"  :avatarURL} />        
-                        </div>
 
-                        <p className={styles.source}>
-                        {source}
-                        </p>  
-                        <p  class={styles.publishedAt}>
-                        { moment(publishedAt || moment.now()).format("hh:mm a")}           
-                        </p>                       
-                        </div>
-  
+                <div className={styles.description}>
+                <div className={eventCategory!="NEWS"?styles.twitterHandlei : styles.hidden} >
+                <img style={{height:30, width:40}} src ={url= "https://s3.ap-south-1.amazonaws.com/public.sicrobo/news-logos/"+source+".jpg" } />
+                </div> 
+                <div className={styles.rightcontainer}>
+                <div className={styles.mainimgheader}>
+                <img  src={urlToImage} className={urlToImage!=null?styles.stretch : styles.hidden} />
+                </div>
 
-                        <div className={styles.description}>
-                        <div className={eventCategory!="NEWS"?styles.twitterHandlei : styles.hidden} >
-                        <img style={{height:30, width:40}} src ={url= "https://s3.ap-south-1.amazonaws.com/public.sicrobo/news-logos/"+source+".jpg" } />
-                        </div> 
-                        <div className={styles.rightcontainer}>
-                        <div className={styles.mainimgheader}>
-                        <img  src={urlToImage} className={urlToImage!=null?styles.stretch : styles.hidden} />
-                        </div>
+                <div className={styles.containertext}>
+                <p className={styles.title} style={{color:'#006EA6'}}>
+                {title}
+                </p>
+                <p className={styles.subtitle}>
+                {subtitle1 }
+                </p>
+                </div>
+                </div>
+                </div>
 
-                        <div className={styles.containertext}>
-                        <p className={styles.title} style={{color:'#006EA6'}}>
-                        {title}
-                        </p>
-                        <p className={styles.subtitle}>
-                        {subtitle1 }
-                        </p>
-                        </div>
-                        </div>
-                        </div>
-
-                        <div className={styles.footer}>
-                        <p className={twitterHandle!=null?styles.twitterHandle : styles.hidden}>@ {twitterHandle}</p>
-                        <p className={styles.url}>
-                        <a className={styles.link} target="_blank" href={twitterurl}>{eventCategory=="NEWS"?"Read More":"Open In Twitter"}</a>
-                        </p>
-                        </div>
-        </div>
-        </div>
+                <div className={styles.footer}>
+                <p className={twitterHandle!=null?styles.twitterHandle : styles.hidden}>@ {twitterHandle}</p>
+                <p className={styles.url}>
+                <a className={styles.link} target="_blank" href={twitterurl}>{eventCategory=="NEWS"?"Read More":"Open In Twitter"}</a>
+                </p>
+                </div>
         </div>
 )       
 export default CustomRow;
