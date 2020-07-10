aca4d1ba3768c982f58ab850f3a4f8032e0f7d1c
iswetak <sweta@ruminationlabs.com>
2020-07-10 19:03:00 +0530

    News_review

--- src/Newscomponent/NewsElement.js
+++ src/Newscomponent/NewsElement.js
@@ -5,16 +5,16 @@ import  styles from "./News.module.css";
 
 const CustomRow = ({ eventCategory,title,subtitle1, urlToImage, avatarURL, publishedAt, source, twitterHandle, twitterurl,url, subtitle3,sourceURL}) => (
         <div className={styles.news_main_container}>
-                <div className={styles.description_container}>
+                <div className={styles.news_container}>
                 <div className={styles.avatar_container}>
                 <img className={styles.avatar} src={url= eventCategory=="NEWS"?"https://s3.ap-south-1.amazonaws.com/public.sicrobo/news-logos/"+source+".jpg"  :avatarURL} />        
                 <div className={eventCategory!="NEWS"?styles.info_twitterHandle : styles.hidden} >
-                <img className={styles.twitterHandle} src ={url= "https://s3.ap-south-1.amazonaws.com/public.sicrobo/news-logos/"+source+".jpg" } />
+                <img className={styles.twitter} src ={url= "https://s3.ap-south-1.amazonaws.com/public.sicrobo/news-logos/"+source+".jpg" } />
                 </div>
                 </div>
                 
-                <div className={styles.right_container}>
-                <div className={styles.title}>
+                <div className={styles.news_description}>
+                <div className={styles.top_header_info}>
                 <p className={styles.source}>{source}</p>
                 <p class={styles.publishedAt}>{moment(publishedAt || moment.now()).format("hh:mm a")}</p>                        
                 </div>
@@ -22,16 +22,19 @@ const CustomRow = ({ eventCategory,title,subtitle1, urlToImage, avatarURL, publi
                 {/* -----------------------news-header-img--------------------- */}
                 
                 <div className={styles.header_image}>
-                <img src={urlToImage} className={urlToImage!=null?styles.stretch : styles.hidden} />
+                <img src={urlToImage} className={urlToImage!=null?styles.image_stretch : styles.hidden} />
                 </div>
 
                 <div className={styles.container_text}>
-                <h2 className={styles.subtitle}>{title}</h2>
+                <h2 className={styles.title_header}>{title}</h2>
                 <p className={styles.subtitle}>{subtitle1}</p>
                 </div>
+               
                 <div className={styles.url_container}>
-                <div className={styles.url}>
-                <p className={twitterHandle!=null?styles.twitterHandle_url : styles.hidden}>@ {twitterHandle}</p>
+                 <div className={styles.twitterHandle_container}>       
+                <p className={twitterHandle!=null?styles.twitterHandleurl : styles.hidden}>@ {twitterHandle}</p>
+                </div>
+                <div>
                 <p className={styles.twitter_url}>
                 <a className={styles.link} target="_blank" href={twitterurl}>{eventCategory=="NEWS"?"Read More":"Open In Twitter"}</a>
                 </p>
